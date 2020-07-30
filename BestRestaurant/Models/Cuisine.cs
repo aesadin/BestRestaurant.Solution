32abf1e159a5594a886c563e679ad6cce9d83ce1
Spencer Moody <spencer.moody@outlook.com>
2020-07-30 10:07:52 -0700

    Add HashSet to Cuisine.cs
    Co-authored-by: Christine Augustine <blacchoy@gmail.com>
    Co-authored-by: aesadin <aesadin@gmail.com>

--- BestRestaurant/Models/Cuisine.cs
+++ BestRestaurant/Models/Cuisine.cs
@@ -6,8 +6,13 @@ namespace BestRestaurant.Models
 {
   public class Cuisine
   {
+    public Cuisine()
+    {
+      this.Restaurants = new HashSet<Restaurant>();
+    }
+
     public int CuisineId { get; set; }
     public string Name { get; set; }
     public virtual ICollection<Restaurant> Restaurants { get; set; } 
-  }
+  }  
 }
