8b20da6ffbb5bcaa84488e180b2c63e2d2b20ba6
Spencer Moody <spencer.moody@outlook.com>
2020-07-30 10:14:18 -0700

    Update BestRestaurantContext
    Co-authored-by: Christine Augustine <blacchoy@gmail.com>
    Co-authored-by: aesadin <aesadin@gmail.com>

--- BestRestaurant/Models/BestRestaurantContext.cs
+++ BestRestaurant/Models/BestRestaurantContext.cs
@@ -4,6 +4,8 @@ namespace BestRestaurant.Models
 {
   public class BestRestaurantContext : DbContext 
   {
-    
+    public virtual DbSet<Cuisine> Cuisines { get; set; }
+    public DbSet<Restaurant> Restaurants { get; set; }
+    public BestRestaurantContext(DbContextOptions options) : base(options) { }
   }
 }
